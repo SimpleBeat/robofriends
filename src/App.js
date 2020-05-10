ba4a6aa8128ed319b50beac6028e5c0edee47638
Илья Катулин <simplebeat@Air.local>
2020-05-10 15:20:39 +0300

    setup state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import SearchBox from './SearchBox'
 import CardList from './CardList'
-import {robots} from './robots';
+import {robots} from './robots'
 
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
     render() {
         return (
-            <CardList robots={robots} />
+            <div className="tc">
+                <h1 className="ma3 pa2 color--blue">Robofriends</h1>
+                <SearchBox />
+                <CardList robots={robots} />
+            </div>
         )
     }
 }
---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SearchBox = () => {
+    return (
+        <div className="ma2 pa2">
+            <input 
+                className="ba pa3 b--green bg-white"
+                type='search' 
+                placeholder='search robot' 
+            />
+        </div>
+    )
+}
+
+export default SearchBox
