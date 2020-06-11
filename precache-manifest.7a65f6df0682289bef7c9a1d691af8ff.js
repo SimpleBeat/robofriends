4855c9fbe6ef6294d9864cf3051990e1042015f8
SimpleBeat <simplebeat@simplebeat.com>
2020-06-11 11:33:42 +0300

    Updates

---
+++ precache-manifest.7a65f6df0682289bef7c9a1d691af8ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62da1e9e201213dbf7a8bea343b8f342",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "dc93320fbacfa125bbdd",
+    "url": "/robofriends/static/css/main.3f9a888b.chunk.css"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc93320fbacfa125bbdd",
+    "url": "/robofriends/static/js/main.c14967d3.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
